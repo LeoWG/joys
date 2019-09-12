fb8dbde717611af0403f28a86495780d767b0b29
wxh <395749329@qq.com>
2019-09-12 15:27:14 +0800

    add some util for this project(ipUtil)

---
+++ src/main/java/com/selffun/joys4fellow/util/IpUtil.java
@@ -0,0 +1,34 @@
+package com.selffun.joys4fellow.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/***
+ * IP工具类
+ */
+
+public class IpUtil {
+    /**
+     * 获取客户端真实IP
+     * @param request request本体
+     * @return 真实ip
+     */
+    public static String getRealIp(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
